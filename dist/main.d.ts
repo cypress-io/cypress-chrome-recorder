af7fd062bb6eb98f037158e9c72242c452b855de
Adam Murray <adamm@cypress.io>
2022-03-23 12:37:41 -0500

    remove dist folder

--- dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-import { Schema } from '@puppeteer/replay';
-export declare function stringifyRecording(parsedRecording: Schema.UserFlow): Promise<Promise<string> | undefined>;
-export default function cypressStringifyChromeRecorder(recordings: string[]): Promise<Promise<string | undefined>[] | undefined>;
