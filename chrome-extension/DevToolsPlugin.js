50b7e68e249c67d0711c22128c7a7f85567d42ce
Adam Murray <adamm@cypress.io>
2022-06-23 10:15:58 -0500

    revert to simple example to get extension running

--- chrome-extension/DevToolsPlugin.js
+++ chrome-extension/DevToolsPlugin.js
@@ -1,17 +1,15 @@
-import { CypressStringifyExtension } from '../src/CypressStringifyExtension';
-import { cypressStringifyChromeRecording } from '../src/main';
-
 export class RecorderPlugin {
   async stringify(recording) {
-    return cypressStringifyChromeRecording(recording, null, 2);
+    return JSON.stringify(recording, null, 2);
   }
   async stringifyStep(step) {
-    return CypressStringifyExtension.stringifyStep(step, null, 2);
+    return JSON.stringify(step, null, 2);
   }
 }
 
-chrome.devtools.recorder.registerRecorderExtensionPlugin(
+/* eslint-disable no-undef */
+chrome.devtools.registerRecorderExtensionPlugin(
   new RecorderPlugin(),
-  'Cypress Test',
-  'application/javascript'
+  /* name=*/ 'Cypress Test',
+  /* mediaType=*/ 'application/javascript'
 );
