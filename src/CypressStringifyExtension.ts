d6674b5e8ee023113efe5fc14086ada9e1820293
Jecelyn Yeen <5917927+jecfish@users.noreply.github.com>
2022-08-18 18:29:12 +0300

    feat(hover): add support for hover

--- src/CypressStringifyExtension.ts
+++ src/CypressStringifyExtension.ts
@@ -50,6 +50,8 @@ export class CypressStringifyExtension extends StringifyExtension {
         return this.#appendNavigationStep(out, step);
       case 'keyDown':
         return this.#appendKeyDownStep(out, step);
+      case 'hover':
+        return this.#appendHoverStep(out, step, flow);
       case 'keyUp':
         return;
       default:
@@ -114,6 +116,20 @@ export class CypressStringifyExtension extends StringifyExtension {
         `Warning: The click on ${step.selectors[0]} was not able to be exported to Cypress. Please adjust your selectors and try again.`
       );
     }
+
+    out.appendLine('');
+  }
+  
+  #appendHoverStep(
+    out: LineWriter,
+    step: Schema.HoverStep,
+    flow: Schema.UserFlow
+  ): void {
+    const cySelector = handleSelectors(step.selectors, flow);
+
+    if (cySelector) {
+      out.appendLine(`${cySelector}.trigger("mouseover");`);
+    } 
 
     out.appendLine('');
   }
