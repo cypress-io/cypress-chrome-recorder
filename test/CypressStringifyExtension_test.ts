b058c8f8746cddd3413e239c8c7112da95a0be15
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-04-19 19:04:59 +0200

    Fix indentation (#53)
    This PR fixes the indentation issues and reworks tests
    to test stringifyStep and stringify using @puppeteer/replay.

--- test/CypressStringifyExtension_test.ts
+++ test/CypressStringifyExtension_test.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { LineWriterImpl } from './LineWriterImpl.js';
+import { stringifyStep, stringify } from '@puppeteer/replay';
 import { CypressStringifyExtension } from '../src/CypressStringifyExtension.js';
 import {
   SupportedRecorderKeysKeys,
@@ -8,8 +8,43 @@ import {
 import { Schema, StepType, AssertedEventType } from '@puppeteer/replay';
 
 describe('CypressStringifyExtension', function () {
+  const extension = new CypressStringifyExtension();
+
+  it('correctly stringifies an entier recording', async function () {
+    const flow = { title: 'click step', steps: [{
+      type: StepType.Click as const,
+      target: 'main',
+      selectors: [['aria/Test'], ['#test']],
+      offsetX: 1,
+      offsetY: 1,
+    }, {
+      type: StepType.Navigate as const,
+      assertedEvents: [
+        {
+          type: AssertedEventType.Navigation as const,
+          url: 'https://learn.cypress.io/',
+          title: 'Coffee cart',
+        },
+      ],
+      url: 'https://learn.cypress.io/',
+    }] };
+
+    const result = await stringify(flow, {
+      extension,
+    });
+
+    assert.equal(result.toString(), `
+describe("click step", () => {
+  it("tests click step", () => {
+    cy.get("#test").click();
+    cy.visit("https://learn.cypress.io/");
+  });
+});
+//# recorderSourceMap=BCBDB
+`.trimStart());
+  });
+
   it('correctly exports Chrome Recorder click step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Click as const,
       target: 'main',
@@ -17,16 +52,14 @@ describe('CypressStringifyExtension', function () {
       offsetX: 1,
       offsetY: 1,
     };
-    const flow = { title: 'click step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
+    const result = await stringifyStep(step, {
+      extension,
+    });
 
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.get("#test").click();\n');
+    assert.equal(result.toString(), 'cy.get("#test").click();\n');
   });
 
   it('correctly exports Chrome Recorder doubleClick step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.DoubleClick as const,
       target: 'main',
@@ -34,16 +67,14 @@ describe('CypressStringifyExtension', function () {
       offsetX: 1,
       offsetY: 1,
     };
-    const flow = { title: 'click step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
+    const result = await stringifyStep(step, {
+      extension,
+    });
 
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.get("#test").dblclick();\n');
+    assert.equal(result.toString(), 'cy.get("#test").dblclick();\n');
   });
 
   it('correctly exports Chrome Recorder click step with right click', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Click as const,
       target: 'main',
@@ -52,16 +83,14 @@ describe('CypressStringifyExtension', function () {
       offsetX: 1,
       offsetY: 1,
     };
-    const flow = { title: 'click step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
+    const result = await stringifyStep(step, {
+      extension,
+    });
 
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.get("#test").rightclick();\n');
+    assert.equal(result.toString(), 'cy.get("#test").rightclick();\n');
   });
 
   it('correctly exports Chrome Recorder navigate step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Navigate as const,
       assertedEvents: [
@@ -73,19 +102,16 @@ describe('CypressStringifyExtension', function () {
       ],
       url: 'https://learn.cypress.io/',
     };
-    const flow = { title: 'navigate step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
+    const result = await stringifyStep(step, {
+      extension,
+    });
 
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(
-      writer.toString(),
+    assert.equal(result.toString(),
       'cy.visit("https://learn.cypress.io/");\n'
     );
   });
 
   it('correctly handles Chrome Recorder click step with asserted navigation', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Click as const,
       target: 'main',
@@ -100,32 +126,27 @@ describe('CypressStringifyExtension', function () {
       offsetX: 1,
       offsetY: 1,
     };
-    const flow = { title: 'click step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
+    const result = await stringifyStep(step, {
+      extension,
+    });
 
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.get("#test").click();\ncy.location("href").should("eq", "https://learn.cypress.io/");\n');
+    assert.equal(result.toString(), 'cy.get("#test").click();\ncy.location("href").should("eq", "https://learn.cypress.io/");\n');
   });
 
   it('correctly exports Chrome Recorder scroll step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Scroll as const,
       target: 'main',
       x: 0,
       y: 805,
     };
-    const flow = { title: 'scroll step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.scrollTo(0, 805);\n');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), 'cy.scrollTo(0, 805);\n');
   });
 
   it('correctly exports Chrome Recorder setViewport step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.SetViewport as const,
       width: 843,
@@ -135,48 +156,40 @@ describe('CypressStringifyExtension', function () {
       hasTouch: false,
       isLandscape: false,
     };
-    const flow = { title: 'setViewport step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.viewport(843, 1041);\n');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), 'cy.viewport(843, 1041);\n');
   });
 
   it('correctly exports Chrome Recorder change step', async function () {
-    const ext = new CypressStringifyExtension();
+    
     const step = {
       type: StepType.Change as const,
       target: 'main',
       selectors: [['aria/Name'], ['#name']],
       value: 'jane',
     };
-    const flow = { title: 'change step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), 'cy.get("#name").type("jane");\n');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), 'cy.get("#name").type("jane");\n');
   });
 
   it('correctly handles keyDown step type', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.KeyDown as const,
       target: 'main',
       key: 'Meta' as const,
     };
-    const flow = { title: 'keyDown step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), '');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), '\n');
   });
 
   it('correctly handles keyDown step types that are supported', async function () {
     Object.keys(supportedRecorderKeys).map(async (key) => {
-      const ext = new CypressStringifyExtension();
       const step = {
         type: StepType.KeyDown as const,
         target: 'main',
@@ -184,57 +197,46 @@ describe('CypressStringifyExtension', function () {
           key as SupportedRecorderKeysKeys
         ].toUpperCase() as Schema.Key,
       };
-      const flow = { title: 'keyUp step', steps: [step] };
-      const writer = new LineWriterImpl('  ');
-
-      await ext.stringifyStep(writer, step, flow);
-
-      assert.equal(writer.toString(), `cy.type("{${key}}");\n`);
+      const result = await stringifyStep(step, {
+        extension,
+      });
+      assert.equal(result.toString(), `cy.type("{${key}}");\n`);
     });
   });
 
   it('correctly handles keyDown step type that are not supported', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.KeyDown as const,
       target: 'main',
       key: 'Meta' as const,
     };
-    const flow = { title: 'keyUp step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), '');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), '\n');
   });
 
   it('correctly handles keyUp step type by ignoring it for now', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.KeyUp as const,
       target: 'main',
       key: 'Meta' as const,
     };
-    const flow = { title: 'keyUp step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), '');
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), '\n');
   });
 
   it('correctly handles Chrome Recorder hover step', async function () {
-    const ext = new CypressStringifyExtension();
     const step = {
       type: StepType.Hover as const,
       target: 'main',
       selectors: [['aria/Test'], ['#test']],
     };
-    const flow = { title: 'hover step', steps: [step] };
-    const writer = new LineWriterImpl('  ');
-
-    await ext.stringifyStep(writer, step, flow);
-
-    assert.equal(writer.toString(), `cy.get("#test").trigger("mouseover");\n`);
+    const result = await stringifyStep(step, {
+      extension,
+    });
+    assert.equal(result.toString(), `cy.get("#test").trigger("mouseover");\n`);
   });
 });
